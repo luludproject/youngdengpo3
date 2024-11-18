c85c607784ef7ef82fa4a6d67d0e59d4cee3af3c
unknown <chlalstn9@gmail.com>
2024-11-18 13:08:57 +0900

    update

--- js/sujww121.js
+++ js/sujww121.js
@@ -1,8 +1,8 @@
 var jsonldData = {
   "@context": "https://schema.org",
   "@type": "Organization",
-  url: "https://gwangjin.jianhomecare.com",
-  logo: "https://gwangjin.jianhomecare.com/img/변기막힘하수구막힘.png",
+  url: "https://nowongu.jianhomecare.com",
+  logo: "https://nowongu.jianhomecare.com/img/변기막힘하수구막힘.png",
 };
 
 var script = document.createElement("script");
