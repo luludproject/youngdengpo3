6d7989dfd3b2b5dff4670fd22a43cd67c451ae86
unknown <chlalstn9@gmail.com>
2024-11-19 20:27:55 +0900

    update

--- js/sujww121.js
+++ js/sujww121.js
@@ -1,8 +1,8 @@
 var jsonldData = {
   "@context": "https://schema.org",
   "@type": "Organization",
-  url: "https://sungdong.jianhomecare.com",
-  logo: "https://sungdong.jianhomecare.com/img/변기막힘하수구막힘.png",
+  url: "https://guro.jianhomecare.com",
+  logo: "https://guro.jianhomecare.com/img/변기막힘하수구막힘.png",
 };
 
 var script = document.createElement("script");
